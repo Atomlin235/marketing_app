d01c04a0c84d7dae9cf4a92e0e5bfebc8638e59d
Atomlin235 <119855164+Atomlin235@users.noreply.github.com>
2025-01-09 21:31:36 +0000

    progress bar

--- pages/3_database_tag_scan.py
+++ pages/3_database_tag_scan.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import base_functions as bf
 import matplotlib.pyplot as plt
+import time
 
 
 def main():
@@ -11,6 +12,15 @@ def main():
     )
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
+        num_rows = df.shape[0]
+        progress_text = "Operation in progress. Please wait."
+        my_bar = st.progress(0, text=progress_text)
+
+        for percent_complete in range(num_rows):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+            time.sleep(1.5)
+            my_bar.empty()
         st.dataframe(df)
         updated_df = bf.add_cms_column(df, 'Website Url', bf.cms_finder_scan)
         st.dataframe(updated_df)
