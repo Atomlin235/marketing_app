7919730750ec14e8d9880ce4e522d8fcbb4e0749
Atomlin235 <119855164+Atomlin235@users.noreply.github.com>
2025-01-09 21:40:48 +0000

    cms counts

--- pages/3_database_tag_scan.py
+++ pages/3_database_tag_scan.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import base_functions as bf
-import matplotlib.pyplot as plt
+
 import time
 
 
@@ -26,22 +26,10 @@ def main():
         st.dataframe(updated_df)
 
         cms_counts = updated_df['CMS'].value_counts()
+        st.dataframe(cms_counts)
 
 # Plot the bar chart
-        plt.figure(figsize=(10, 6))
-        cms_counts.plot(kind='bar')
-
-        # Customize the plot
-        plt.title("Count of Each CMS In Base", fontsize=16)
-        plt.xlabel("CMS", fontsize=14)
-        plt.ylabel("Count", fontsize=14)
-        plt.xticks(rotation=45)
-        plt.grid(axis='y', linestyle='--', alpha=0.7)
-
-        # Show the plot
-        plt.tight_layout()
-        plt.show()
-
+        
 
     
 if __name__ == "__main__":
